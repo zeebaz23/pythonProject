ce7becb8f0113ed72ace54613fe736bd68995f48
zeebaz23 <sebastianaguiar462@gmail.com>
2024-02-15 22:36:18 -0500

    add exercise one and two

---
+++ Ejercicios.py
@@ -0,0 +1,13 @@
+# Ejercicio 1
+nombre = input("Ingrese su nombre:")
+edad = int(input("Ingrese su edad:"))
+print(f"Hola {nombre} su edad es {edad}")
+
+# Ejercicio 2
+import math
+radio = float(input("ingrese el radio del circulo"))
+Area = math.pi * radio**2
+print(f"el area del circulo es {Area}")
+
+# Ejercicio 3
+
